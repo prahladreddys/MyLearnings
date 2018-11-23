c0ef4fb3c7ef7bbf731a7d0ac0f76a926fe932fa
PRAHLAD <psankepally@ciphercloud.com>
2018-11-23 10:11:24 +0530

    Updated

--- SimpleService/src/main/java/com/prahlad/rest/service/SimpleService.java
+++ SimpleService/src/main/java/com/prahlad/rest/service/SimpleService.java
@@ -3,6 +3,8 @@ package com.prahlad.rest.service;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +16,14 @@ public class SimpleService {
 	}
 	@RequestMapping("/next")
 	public String getNext(){
-		return RandomStringUtils.randomAlphabetic(3);
+		return RandomStringUtils.randomAlphabetic(10);
 	}
 	public static void main(String[] args) {
 		SimpleService m = new SimpleService();
 		System.out.println(m.getNext());
+	}
+	@RequestMapping(value="/companies",method=RequestMethod.GET,produces="application/json")
+	public String getCom(@RequestParam(value="city",defaultValue="hyd") String city){
+		return "welcome "+city;
 	}
 }
